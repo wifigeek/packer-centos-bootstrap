cfbf2a9d18546a8dd332712196c0ab47c0e16a8b
Philip Gaw <pgaw+git@tuxgrid.com>
2017-02-10 11:47:56 +0000

    fix for salt minion install

--- scripts/salt-minion.sh
+++ scripts/salt-minion.sh
@@ -1,5 +1,5 @@
 # Install the salt minion
 
 curl -o bootstrap_salt.sh -L https://bootstrap.saltstack.com
-sudo sh bootstrap_salt.sh -G -Z stable -A 10.102.128.128
+sudo sh bootstrap_salt.sh -Z -A 10.102.128.128 stable
 rm -f bootstrap_salt.sh
